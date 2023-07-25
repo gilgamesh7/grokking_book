eeb56b408d9a5e4cbc09c05a98fd6b3561f8c2ef
gilgamesh7 <felidaesrule@yahoo.com>
2023-07-25 20:16:44 +1200

    Add binary_search skeleton function

--- binary_search.py
+++ binary_search.py
@@ -1,9 +1,13 @@
 import numpy as np
 
 import logging
+from typing import List
 
 logging.basicConfig(level=logging.INFO, format="[{asctime}] - {funcName} - {lineno} - {message}", style='{')
 logger = logging.getLogger("grokking")
+
+def binary_search(array:List[int], search_for_this_number:int)->str:
+    return "Found"
 
 if __name__ == "__main__":
     try:
@@ -14,5 +18,9 @@ if __name__ == "__main__":
         random_int_array = np.sort(np.random.choice(np.arange(1,101), size=20, replace=False)).tolist()
         
         logger.info(f"Random Number Array : \n {random_int_array}")
+
+        search_for_this_number = input("Enter a number between 1 and 100 to search :")
+
+        logger.info(f"Search Result : {search_for_this_number} is {binary_search(random_int_array, search_for_this_number)} in array {random_int_array}")
     except Exception as error:
         logger.error(f"{type(error).__name__} - {error}")

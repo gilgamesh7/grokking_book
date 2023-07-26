72cfbee7889022d3d2357c92660b0a58cb0fb57f
gilgamesh7 <felidaesrule@yahoo.com>
2023-07-26 20:27:44 +1200

    Add timer

--- binary_search.py
+++ binary_search.py
@@ -1,11 +1,25 @@
 import numpy as np
 
 import logging
-from typing import List
+from typing import List, Callable
+import timeit
 
 logging.basicConfig(level=logging.INFO, format="[{asctime}] - {funcName} - {lineno} - {message}", style='{')
 logger = logging.getLogger("grokking")
 
+
+def calculate_elapsed_time(in_func: Callable)-> Callable:
+    def calculate_timer(*args, **kwargs)-> int:
+        start = timeit.default_timer()
+        value = in_func(*args)
+        end = timeit.default_timer()
+        elapsed_time = end-start
+
+        return value, elapsed_time
+    
+    return calculate_timer
+    
+@calculate_elapsed_time
 def binary_search(array:List[int], search_for_this_number:int)->str:
     return "Found"
 
@@ -21,6 +35,8 @@ if __name__ == "__main__":
 
         search_for_this_number = input("Enter a number between 1 and 100 to search :")
 
-        logger.info(f"Search Result : {search_for_this_number} is {binary_search(random_int_array, search_for_this_number)} in array {random_int_array}")
+        result, time_taken = binary_search(random_int_array, search_for_this_number)
+
+        logger.info(f"Search Result : {search_for_this_number} is {result} in array {random_int_array} and this took {time_taken} seconds")
     except Exception as error:
         logger.error(f"{type(error).__name__} - {error}")

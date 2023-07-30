5966915d3e298bbdaf96e4f8eaa7fa5df1652d7e
gilgamesh7 <felidaesrule@yahoo.com>
2023-07-30 12:01:45 +1200

    Add numner of steps

--- binary_search.py
+++ binary_search.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 
 import logging
-from typing import List, Callable
+from typing import List, Callable, Union
 import timeit
 
 logging.basicConfig(level=logging.INFO, format="[{asctime}] - {funcName} - {lineno} - {message}", style='{')
@@ -12,34 +12,49 @@ logger = logging.getLogger("grokking")
 def calculate_elapsed_time(in_func: Callable)-> Callable:
     def calculate_timer(*args, **kwargs)-> int:
         start = timeit.default_timer()
-        value = in_func(*args)
+        steps, value = in_func(*args)
         end = timeit.default_timer()
         elapsed_time = end-start
 
-        return value, elapsed_time
+        return steps, value, elapsed_time
     
     return calculate_timer
-    
+
 @calculate_elapsed_time
-def binary_search(input_number_array:List[int], search_for_this_number:int)->str:
+def linear_search(input_number_array:List[int], search_for_this_number:int)->Union[int,str]:
+    try:
+        number_of_steps = 0
+        for number in input_number_array:
+            number_of_steps += 1
+            if number == search_for_this_number:
+                return number_of_steps, "Found"
+            
+        return number_of_steps, "Not Found"
+
+    except Exception as error:
+        raise error
+
+@calculate_elapsed_time
+def binary_search(input_number_array:List[int], search_for_this_number:int)->Union[int,str]:
     try:
         # keep track of which part of the list you'll search in.
         low = 0
         high = len(input_number_array)-1
+        number_of_steps = 0
 
         while low <= high :
+            number_of_steps += 1
             mid = math.floor((low + high)/2)    # check the middle element.
-            print(mid, type(mid))
             guess = input_number_array[mid]
 
             if guess == search_for_this_number:
-                return "Found"
+                return number_of_steps, "Found"
 
             if guess > search_for_this_number :
                 high = mid - 1            
             else:
                 low = mid + 1
-        return "Not Found"
+        return number_of_steps, "Not Found"
     except Exception as error:
         raise error
 
@@ -55,8 +70,10 @@ if __name__ == "__main__":
 
         search_for_this_number = int(input("Enter a number between 1 and 100 to search :"))
 
-        result, time_taken = binary_search(random_int_array, search_for_this_number)
+        number_of_steps, result, time_taken = binary_search(random_int_array, search_for_this_number)
+        logger.info(f"Binary Search Result : {search_for_this_number} is {result} , took {number_of_steps} steps and this took {time_taken:.10f} seconds")
 
-        logger.info(f"Search Result : {search_for_this_number} is {result} in array {random_int_array} and this took {time_taken} seconds")
+        number_of_steps, result, time_taken = linear_search(random_int_array, search_for_this_number)
+        logger.info(f"Linear Search Result : {search_for_this_number} is {result} , took {number_of_steps} steps and this took {time_taken:.10f} seconds")
     except Exception as error:
         logger.error(f"{type(error).__name__} - {error}")
